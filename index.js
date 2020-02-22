b18a7b698d92afaf238b540f9b8d72504d194bec
Misha <m_poluektov@ukr.net>
2020-02-22 11:46:51 +0200

    commit

--- index.js
+++ index.js
@@ -1,41 +1,14 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const router = require('./routes/index');
+
+const PORT = 8080
 const app = express();
 
 app.use(express.urlencoded({extended: true})); //parse body
 app.use(express.json());
+app.use(router);
 
-app.get('/',  (req, res) => {
-  res.send('TEst');
-});
 
-app.get('/user', (req, res) => { //request whith query params
-  const query = req.query;
-  console.log(query);
-  res.send(query);
-})
-
-app.get('/user/:id', (req, res) => {
-  const id = req.params.id;
-  res.send(`get ${id} user`);
-})
-
-app.post('/user', (req, res) => {
-  const body = req.body;
-  console.log(body);
-  res.send(body);
-})
-
-app.put('/user', (req, res) => {
-  console.log(req.headers.authorization)
-  res.send('put user');
-})
-
-app.delete('/user', (req, res) => {
-  res.send('delete user');
-})
-
-const PORT = 8080
 
 app.listen(PORT, () => {
   console.log('Example app listening on port ' + PORT);
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+
+const router = express.Router();
+
+router.get('/user', userController.getAllUsers);
+router.get('/user/:id', userController.getUserById);
+router.post('/user', userController.createUser);
+router.put('/user', userController.updateUser);
+router.delete('/user', userController.deleteUser);
+
+module.exports = router;
