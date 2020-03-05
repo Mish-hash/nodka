25e9522de4fa2c46044f8dea84c46b7c72955926
Mish-hash <poluektov23@gmail.com>
2020-03-05 20:35:53 +0200

    install mongoDB and studio3t, after runing project and all components

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = require('./routes/index');
 require('./dbConnection');
 
-const PORT = 8080
+const PORT = 8090
 const app = express();
 
 app.use(express.urlencoded({extended: true})); //parse body
